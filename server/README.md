57d6408521a35421b4fab776a3aab7a4f5759bbe
Limbo Peng <iwinux@gmail.com>
2012-09-18 21:43:01 +0800

    initialized rails project

---
+++ server/README.md
@@ -0,0 +1,9 @@
+# EyesOn App 服务端
+
+## 开发环境搭建
+
+To be added.
+
+## 服务器部署
+
+To be added.
---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyesOn::Application.config.secret_token = '17a36d228caa5559b259897e09a01ab8d0774da0de6c71978142d89117dcd1f7d0873c15e31ce09f88945264e8d325afa65160c0d062028cf142204885c3d740'
